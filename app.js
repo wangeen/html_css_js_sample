37b24be78811de99454914884347cdbb33be8a75
wangeen <wangeen@gmail.com>
2014-09-15 12:14:00 +0800

    initial sample html css js sample

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+function MyApp()
+{
+        var version = "v1.0";
+        function setStatus(message)
+        {
+                $("#app>footer").text(message);
+        }
+        this.start = function()
+        {
+                $("#app>header").append(version);
+                setStatus("ready");
+        };
+}
+
+var myApp = {
+        version: "v1.0",
+        setStatus: function(message)
+        {
+                $("#app>footer").text(message);
+        },
+        start: function()
+        {
+                $("#app>header").append(this.version);
+                this.setStatus("ready");
+        };
+};
+
+$(function() {
+        window.app = new MyApp(); 
+        window.app.start(); 
+}); 
+
