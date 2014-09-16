9021155ba682fb8d3c11b97b9a73568f739c06c2
wangeen <wangeen@gmail.com>
2014-09-16 10:35:29 +0800

    stack sample add delete up down

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
 "use strict";
+function addTask(){ // add task to list
+    var newItem = $("#new-task-name").val(); 
+    if(newItem){
+        var $task = $("<li></li>"); 
+        var $delete = $("<button class='delete'>X</button>"); 
+        var $moveUp = $("<button class='moveUp'>^</button>"); 
+        var $moveDown = $("<button class='moveDown'>V</button>"); 
+        $task.append($delete)
+            .append($moveUp)
+            .append($moveDown)
+            .append("<span class='task-name'>"+newItem+"</span>"); 
+        $delete.click( // very tricky,  how can delete parent DOM
+            function(){
+                $task.remove(); 
+            }
+        ); 
+        $moveUp.click(
+            function(){
+                $task.insertBefore($task.prev()); 
+            }
+        )
+        $moveDown.click(
+            function(){
+                $task.insertAfter($task.next()); 
+            }
+        )
+
+        $("#task-list").append($task); 
+
+        $("#new-task-name").val("").focus(); 
+    }
+}
+
 function MyApp()
 {
-        var version = "v1.0";
-        function setStatus(message)
-        {
-                $("#app>footer").text(message);
-        }
-        this.start = function()
-        {
-                $("#app>header").append(version);
-                setStatus("ready");
-        };
+    var version = "v1.0";
+    function setStatus(message)
+    {
+        $("#app>footer").text(message);
+    }
+    this.start = function()
+    {
+        $("#new-task-name").keypress(
+            function(e){
+                if(e.which==13){ // 13: Enter pressed
+                    addTask(); 
+                    return false; 
+                }
+            }
+        ).focus(); 
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
 }
 
 //var myApp = {
