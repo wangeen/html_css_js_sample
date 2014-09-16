404b4d40aa43c62f220679766f2fb72f2f617509
wangeen <wangeen@gmail.com>
2014-09-16 15:06:18 +0800

    to keep code simple, copy hide template instead of all from javascript

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
 "use strict";
 function addTask(){ // add task to list
-    var newItem = $("#new-task-name").val(); 
-    if(newItem){
-        var $task = $("<li></li>"); 
-        var $delete = $("<button class='delete'>X</button>"); 
-        var $moveUp = $("<button class='moveUp'>^</button>"); 
-        var $moveDown = $("<button class='moveDown'>V</button>"); 
-        $task.append($delete)
-            .append($moveUp)
-            .append($moveDown)
-            .append("<span class='task-name'>"+newItem+"</span>"); 
-        $delete.click( // very tricky,  how can delete parent DOM
-            function(){
-                $task.remove(); 
-            }
-        ); 
-        $moveUp.click(
-            function(){
-                $task.insertBefore($task.prev()); 
-            }
-        )
-        $moveDown.click(
-            function(){
-                $task.insertAfter($task.next()); 
-            }
-        )
+    var taskName = $("#new-task-name").val(); 
+    if(taskName == 0) return; 
+    // important the space between template and task must keep
+    var $task= $("#task-template .task").clone()
+    $("span.task-name", $task).text(taskName); 
+    $("#task-list").append($task); 
 
-        $("#task-list").append($task); 
+    $("button.delete", $task).click(
+        function(){
+            $task.remove();     
+        }
+    )
+    $("button.move-up", $task).click(
+        function(){
+            $task.insertBefore($task.prev());     
+        }
+    )
+    $("button.move-down", $task).click(
+        function(){
+            $task.insertAfter($task.next());     
+        }
+    )
 
-        $("#new-task-name").val("").focus(); 
-    }
+
+//    var newItem = $("#new-task-name").val(); 
+//    if(newItem){
+//        var $task = $("<li></li>"); 
+//        var $delete = $("<button class='delete'>X</button>"); 
+//        var $moveUp = $("<button class='moveUp'>^</button>"); 
+//        var $moveDown = $("<button class='moveDown'>V</button>"); 
+//        $task.append($delete)
+//            .append($moveUp)
+//            .append($moveDown)
+//            .append("<span class='task-name'>"+newItem+"</span>"); 
+//        $delete.click( // very tricky,  how can delete parent DOM
+//            function(){
+//                $task.remove(); 
+//            }
+//        ); 
+//        $moveUp.click(
+//            function(){
+//                $task.insertBefore($task.prev()); 
+//            }
+//        )
+//        $moveDown.click(
+//            function(){
+//                $task.insertAfter($task.next()); 
+//            }
+//        )
+//
+//        $("#task-list").append($task); 
+//
+//        $("#new-task-name").val("").focus(); 
+//    }
 }
 
 function MyApp()
