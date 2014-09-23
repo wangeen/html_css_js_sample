6d9d55648620a976142bb9396e2b8a7c0d768f73
wangeen <wangeen@gmail.com>
2014-09-23 16:28:25 +0800

    temp

--- app.js
+++ app.js
@@ -1,63 +1,46 @@
 "use strict";
-function addTask(){ // add task to list
-    var taskName = $("#new-task-name").val(); 
-    if(taskName == 0) return; 
-    // important the space between template and task must keep
-    var $task= $("#task-template .task").clone()
-    $("span.task-name", $task).text(taskName); 
-    $("#task-list").append($task); 
-
-    $("button.delete", $task).click(
-        function(){
-            $task.remove();     
-        }
-    )
-    $("button.move-up", $task).click(
-        function(){
-            $task.insertBefore($task.prev());     
-        }
-    )
-    $("button.move-down", $task).click(
-        function(){
-            $task.insertAfter($task.next());     
-        }
-    )
-
-
-//    var newItem = $("#new-task-name").val(); 
-//    if(newItem){
-//        var $task = $("<li></li>"); 
-//        var $delete = $("<button class='delete'>X</button>"); 
-//        var $moveUp = $("<button class='moveUp'>^</button>"); 
-//        var $moveDown = $("<button class='moveDown'>V</button>"); 
-//        $task.append($delete)
-//            .append($moveUp)
-//            .append($moveDown)
-//            .append("<span class='task-name'>"+newItem+"</span>"); 
-//        $delete.click( // very tricky,  how can delete parent DOM
-//            function(){
-//                $task.remove(); 
-//            }
-//        ); 
-//        $moveUp.click(
-//            function(){
-//                $task.insertBefore($task.prev()); 
-//            }
-//        )
-//        $moveDown.click(
-//            function(){
-//                $task.insertAfter($task.next()); 
-//            }
-//        )
-//
-//        $("#task-list").append($task); 
-//
-//        $("#new-task-name").val("").focus(); 
-//    }
-}
 
 function MyApp()
 {
+    function removeTask($task){
+        $task.remove();     
+    }
+    
+    function moveTask($task, moveUp){
+        if(moveUp){
+            $task.insertBefore($task.prev());     
+        }else{
+            $task.insertAfter($task.next());     
+        }
+    }
+
+    function addTask(){ // add task to list
+        var taskName = $("#new-task-name").val(); 
+        if(taskName == 0) return; 
+        // important the space between template and task must keep
+        var $task= $("#task-template .task").clone()
+        $("span.task-name", $task).text(taskName); 
+        $("#task-list").append($task); 
+        var msg = $("#task-list").html();
+        console.log(msg);
+
+        $("button.delete", $task).click(
+            function(){
+                removeTask($task);
+            }
+        )
+        $("button.move-up", $task).click(
+            function(){
+                moveTask($task,true);
+            }
+        )
+        $("button.move-down", $task).click(
+            function(){
+                moveTask($task,false);
+            }
+        )
+    }
+
     var version = "v1.0";
     function setStatus(message)
     {
---
+++ hh
@@ -0,0 +1,8 @@
+* 7a85f2b (master) testing version for storage, still not work
+| * a4bfb8f (try_fix_bug) temp
+|/  
+* 404b4d4 (origin/master, origin/HEAD) to keep code simple, copy hide template instead of all from javascript
+* 9021155 stack sample add delete up down
+* e706f71 js work version
+* 37b24be initial sample html css js sample
+* bcd8005 Initial commit
