fbd17158d2334ef47ad03613dce9cf9df855b843
wangeen <wangeen@gmail.com>
2014-09-23 17:38:39 +0800

    local storage work

--- app.js
+++ app.js
@@ -2,6 +2,27 @@
 
 function MyApp()
 {
+    var appStorage = new AppStorage("task-name");
+    function saveTaskList(){
+        var tasks = [];
+        $("#task-list .task span.task-name").each(function(){
+            tasks.push($(this).text())
+            console.log("save: "+ $(this).text());
+        });
+        appStorage.setValue("taskList", tasks);
+    }
+
+    function loadTaskList(){
+        var tasks = appStorage.getValue("taskList");
+        if(tasks){
+            for(var i in tasks){
+                console.log("load: "+tasks[i]);
+                addTask(tasks[i]);
+            }
+        }
+    }
+
+
     function removeTask($task){
         $task.remove();     
     }
@@ -14,8 +35,10 @@ function MyApp()
         }
     }
 
-    function addTask(){ // add task to list
-        var taskName = $("#new-task-name").val(); 
+    function addTask(taskName){ // add task to list
+        if (taskName === undefined){
+            taskName = $("#new-task-name").val(); 
+        }
         if(taskName == 0) return; 
         // important the space between template and task must keep
         var $task= $("#task-template .task").clone()
@@ -23,6 +46,7 @@ function MyApp()
         $("#task-list").append($task); 
         var msg = $("#task-list").html();
         console.log(msg);
+        saveTaskList(); 
 
         $("button.delete", $task).click(
             function(){
@@ -56,20 +80,9 @@ function MyApp()
                 }
             }
         ).focus(); 
+        loadTaskList(); 
         $("#app>header").append(version);
         setStatus("ready");
     };
 }
 
-//var myApp = {
-//        version: "v1.0",
-//        setStatus: function(message)
-//        {
-//                $("#app>footer").text(message);
-//        },
-//        start: function()
-//        {
-//                $("#app>header").append(this.version);
-//                this.setStatus("ready");
-//        };
-//};
